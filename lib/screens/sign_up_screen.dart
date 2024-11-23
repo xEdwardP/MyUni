99e67c04cf09fdba89865184b629808e662fdce7
JoseAlvarado <boanerges5orellana@gmail.com>
2024-11-23 14:00:49 -0600

    Change to stekch

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
-//clase de registro
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUpScreen(),
+    );
+  }
+}
+
 class SignUpScreen extends StatelessWidget {
- 
- static String id = "SingUpPage";
-
- @override
-
+  @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
@@ -15,113 +25,88 @@ class SignUpScreen extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              
-              Row(
-        children: [
-            Text("BIBLIOTECA UNICAH",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-             Text("Crea una Cuenta",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Color.fromARGB(255, 0, 0, 0),
-                fontSize: 25,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-        ]  
-              ),
-              
-           //metodos de los textbox
-              SizedBox(height: 25.0,),
-              _textFleldName(),
-               SizedBox(height: 15.0,),
-              _textFleldEmail(),
-              SizedBox(height: 15.0,),
-              _textFleldPassword(),
-             SizedBox(height: 15.0,),
-             Row(
-              children: [
-                  Text('¿Ya tienes una cuenta?',
+              Column(
+                children: [
+                  Text(
+                    "BIBLIOTECA UNICAH",
+                    textAlign: TextAlign.center,
                     style: TextStyle(
-                      color: Colors.amber[50],
-                      fontSize: 20,
+                      color: Colors.black,
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  _buttonsLogin(),
-              ],
-             )
-             
-    
+                  SizedBox(height: 10),
+                  Text(
+                    "Crea una Cuenta",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 25.0),
+              _textFleldName(),
+              SizedBox(height: 15.0),
+              _textFleldEmail(),
+              SizedBox(height: 15.0),
+              _textFleldPassword(),
+              SizedBox(height: 15.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    '¿Ya tienes una cuenta?',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Inicia Sesión',
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  ),
+                ],
+              ),
             ],
           ),
         ),
       ),
     );
   }
-  //metodo del nombre
-  Widget _textFleldName() {
 
+  Widget _textFleldName() {
     return _TextFieldGeneral(
       labelText: 'Nombre',
       hintText: 'Jose Alvarado',
-      onChanged: (value){},
+      onChanged: (value) {},
     );
-    
   }
-  //metodo del email
-  
- Widget _textFleldEmail() {
-  return _TextFieldGeneral(
-    labelText: 'Email',
-    hintText: 'Example@gmail.com',
-    keyboardType: TextInputType.emailAddress,
-    onChanged: (value){},
-    icon: Icons.email_outlined,
-     );
- }
- //metodo de la contraseña
-  
- Widget _textFleldPassword() {
-  return _TextFieldGeneral(
-    labelText: 'Contraseña',
-    onChanged: (value){},
-    icon: Icons.lock_outline_rounded,
-    obscuretext: true,
-  );
- }
- 
- //metodo del boton de inicio de sesion
-  Widget _buttonsLogin() {
-    return ElevatedButton(
-      style: ElevatedButton.styleFrom(
-        backgroundColor: const Color.fromARGB(0, 255, 255, 255), 
-        foregroundColor: Colors.white,
-        padding: const EdgeInsets.symmetric(horizontal: 110.0,vertical: 10),
-        shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-        ),
-      ),
-      
-      onPressed: (){
-    
-      }, 
 
-    child: Text('Inicia Sesión',
-    style: TextStyle(
-    color: Colors.amber[50],
-    fontSize: 20,
-      ),
-    ),
-
-    ); 
+  Widget _textFleldEmail() {
+    return _TextFieldGeneral(
+      labelText: 'Email',
+      hintText: 'Example@gmail.com',
+      keyboardType: TextInputType.emailAddress,
+      onChanged: (value) {},
+      icon: Icons.email_outlined,
+    );
   }
-  
+
+  Widget _textFleldPassword() {
+    return _TextFieldGeneral(
+      labelText: 'Contraseña',
+      onChanged: (value) {},
+      icon: Icons.lock_outline_rounded,
+      obscuretext: true,
+    );
+  }
 }
 
 class _TextFieldGeneral extends StatelessWidget {
@@ -133,32 +118,26 @@ class _TextFieldGeneral extends StatelessWidget {
   final bool obscuretext;
 
   const _TextFieldGeneral({
-   required this.labelText,
-    this.hintText='',
+    required this.labelText,
+    this.hintText = '',
     required this.onChanged,
     this.keyboardType,
     this.icon,
-    this.obscuretext=false,
-
+    this.obscuretext = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(
-        horizontal: 30.0,
-      ),
-       color: Colors.white,
-       decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-       ),
+      margin: EdgeInsets.symmetric(horizontal: 30.0),
       child: TextField(
         keyboardType: keyboardType,
         obscureText: obscuretext,
         decoration: InputDecoration(
-          prefixIcon: Icon(icon),
-          labelText: "labelText",
-          hintText: "hintText",
+          prefixIcon: icon != null ? Icon(icon) : null,
+          labelText: labelText,
+          hintText: hintText,
+          border: OutlineInputBorder(),
         ),
         onChanged: onChanged,
       ),
