bd2abc56b906e4592d51b225d7afd1efe39f6f17
xEdwardP <112675355+xEdwardP@users.noreply.github.com>
2024-11-24 00:17:06 -0600

    Add Sign Up

---
+++ lib/screens/attendance_student_screen.dart
@@ -0,0 +1,116 @@
+
+import 'package:flutter/material.dart';
+
+class AttendanceStudent extends StatefulWidget {
+  const AttendanceStudent({super.key});
+
+  @override
+  State<AttendanceStudent> createState() => _AttendanceStudentState();
+}
+
+
+//Assittence of Students
+class _AttendanceStudentState extends State<AttendanceStudent> {
+final TextEditingController _name = TextEditingController();
+final TextEditingController _id = TextEditingController();
+
+String? _entrada;
+String? _salida;
+
+//Mark to Time of Entry & Leave
+void _marcaEntrada(){
+  setState(() {
+    _entrada = TimeOfDay.now().format(context);
+  });
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text('Hora de entrada: $_entrada')),
+  );
+}
+void _marcaSalida(){
+  setState(() {
+  _salida =TimeOfDay.now().format(context);    
+  });
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text('Hora de salida: $_salida')),
+  );
+}
+
+//Screen
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      
+      backgroundColor: const Color.fromARGB(255, 220, 255, 208),
+      body: Center(
+        
+        child:  Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Text(
+                  'REGISTRO DE ASISTENCIA',
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.indigo,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                  TextField(
+              controller: _name,
+              decoration: InputDecoration(
+                labelText: 'Nombre',
+                hintText: 'Jose Alvarado',
+                border: OutlineInputBorder(),
+                icon: Icon(Icons.person_2_outlined),                
+              ),
+              
+            ),
+            SizedBox(height: 15),
+            TextField(
+              controller: _id,
+              decoration: InputDecoration(
+                labelText: 'Cuenta',
+                hintText: '0000200500000',
+                border: OutlineInputBorder(),
+                icon: Icon(Icons.account_circle_outlined)                
+              ),
+
+
+              //Button for Mark
+          keyboardType: TextInputType.number,
+            ),  
+            SizedBox(height: 15),
+            ElevatedButton(onPressed: _marcaEntrada,
+            child:  Text('Marcar Entrada'),
+             ),
+             SizedBox(height: 15),
+             ElevatedButton(onPressed: _marcaSalida,
+              child: Text('Marcar Salida')
+              ),
+              SizedBox(height: 10),
+            if(_entrada !=null)
+            Text('Hora de entrada $_entrada',
+            style: TextStyle(fontSize: 15),
+              ),
+              if(_salida != null)
+              Text('Hora de salida $_salida',
+              style: TextStyle(fontSize: 15),
+              ),
+
+
+              ],
+            )
+          
+
+          ],
+        ),
+         ),
+      ),
+     );
+  }
+
+  
+}
